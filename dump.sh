c19d1118c724d8f08350dfd74929b67ebe3ffc3d
Shinsuke Nishio <nishio@densan-labs.net>
2018-08-26 20:11:21 +0900

    add

--- dump.sh
+++ dump.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-protoc -I ./proto/ --plugin=protoc-gen-reqdump --reqdump_out=./dump/ test_message.proto 
+protoc -I ./proto/ --plugin=protoc-gen-reqdump --reqdump_out=./dump/ test_message.proto other_message.proto
